f52da092bc214f1eab21f003903cc76f0d954c69
Rafi Khelalfa <khelalfa.rafi2013@gmail.com>
2026-08-27 20:09:14 +0200

    Update energy_estimator.py

--- ecoedge/profiling/energy_estimator.py
+++ ecoedge/profiling/energy_estimator.py
@@ -1,1 +1,6 @@
+class EnergyEstimator:
 
+    @staticmethod
+    def estimate_joules(latency_ms: float, power_watts: float = 2.5) -> float:
+        latency_seconds = latency_ms / 1000.0
+        return latency_seconds * power_watts
