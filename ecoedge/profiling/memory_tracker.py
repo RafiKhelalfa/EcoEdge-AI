31d13ae185b3e149efc278a94c3ad53f2c5a1355
Rafi Khelalfa <khelalfa.rafi2013@gmail.com>
2026-08-27 20:08:32 +0200

    Update memory_tracker.py

--- ecoedge/profiling/memory_tracker.py
+++ ecoedge/profiling/memory_tracker.py
@@ -1,1 +1,21 @@
+import os
+import psutil
+import torch
 
+
+class MemoryTracker:
+
+    @staticmethod
+    def get_process_memory_mb() -> float:
+        process = psutil.Process(os.getpid())
+        return process.memory_info().rss / (1024 * 1024)
+
+    @staticmethod
+    def get_model_size_mb(model: torch.nn.Module) -> float:
+        param_size = 0
+        for param in model.parameters():
+            param_size += param.nelement() * param.element_size()
+        buffer_size = 0
+        for buffer in model.buffers():
+            buffer_size += buffer.nelement() * buffer.element_size()
+        return (param_size + buffer_size) / (1024 * 1024)
