cf1639c3c95a43afb5d17706846a2b6cbc0ccb79
Rafi Khelalfa <khelalfa.rafi2013@gmail.com>
2026-09-01 13:39:53 +0200

    Update __init__.py

--- ecoedge/compression/__init__.py
+++ ecoedge/compression/__init__.py
@@ -1,5 +1,6 @@
 from .pruner import StructuredPruner
 from .quantizer import ModelQuantizer
 from .distiller import KnowledgeDistiller
+from .pruning import Pruner
 
 __all__ = ["StructuredPruner", "ModelQuantizer", "KnowledgeDistiller"]
